9a01fc95da6bbcdaa8ee0d36b9e56ca73086d249
kim-yu <kimberly.yu1@gmail.com>
2018-03-16 15:44:32 -0400

    modify program for program 2.0

--- README.md
+++
@@ -1,1 +0,0 @@
-# tiny-computer-generated-booklet
--- a_short_guide_to_internet_slang.py
+++ a_short_guide_to_internet_slang.py
@@ -1,54 +1,66 @@
-from nltk.corpus import words
 import random
 
-internet_slang = {  'afk': 'away from keyboard',
-                    'ama': 'ask me anything',
-                    'asap': 'as soon as possible',
-                    'atm': 'at the moment',
+internet_slang = {  'aab': 'average at best',
+                    'aak': 'alive and kicking',
+                    'aap': 'always a pleasure',
+                    'abd': 'already been done',
                     'bff': 'best friends forever',
-                    'btw': 'by the way', 
+                    'bts': 'be there soon',
+                    'ctn': 'cannot talk now',
+                    'cye': 'check your email',
+                    'diy': 'do it yourself',
                     'dnd': 'do not disturb',
-                    'faq': 'frequently asked questions', 
+                    'eom': 'end of message',
+                    'etw': 'enjoy the weekend',
+                    'fka': 'formerly known as',
                     'ftw': 'for the win',
-                    'fyi': 'for your information', 
-                    'gtg': 'got to go',
-                    'idk': "i don't know",
+                    'giy': 'google it yourself',
+                    'gnd': 'girl next door',
+                    'gsd': 'getting stuff done',
+                    'gtb': 'go to bed',
+                    'hak': 'hugs and kisses',
+                    'hth': 'hope this helps',
+                    'idc': "i don't care",
                     'ily': 'i love you',
                     'imo': 'in my opinion',
                     'irl': 'in real life', 
-                    'jk': 'just kidding',
-                    'lmgtfy': 'let me google that for you',
+                    'jam': 'just a minute',
+                    'jtc': 'join the club',
+                    'jwd': 'job well done',
                     'lmk': 'let me know',
                     'lol': 'laugh out loud', 
-                    'myob': 'mind your own business',
-                    'np': 'no problem',
-                    'nsfw': 'not safe for work', 
-                    'omw': 'on my way',
+                    'nrn': 'no reply necessary',
+                    'otl': 'out to lunch',
+                    'otp': 'on the phone',
+                    'pir': 'parent in room',
                     'smh': 'shaking my head',
-                    'tbh': 'to be honest', 
-                    'tldr': "too long didn't read",
+                    'tia': 'thanks in advance',
                     'tmi': 'too much information',
-                    'ttyl': 'talk to you later',
-                    'ty': 'thank you',
-                    'yolo': 'you only live once'
+                    'tyt': 'take your time',
+                    'wfm': 'works for me',
+                    'yam': 'yet another meeting'
                 }
-
 slang = random.choice([a for a in internet_slang.keys()])
-meaning = internet_slang[slang].split()
+print(slang)
 
-new_meaning = ''
-real_meaning = ''
+definition = internet_slang[slang].split()
+new_definition = ''
+real_definition = ''
 for i in range(len(slang)):
-    w = random.choice([s for s in words.words("en-basic") if s[0] == slang[i].lower() and s != meaning[i] and s not in new_meaning])
-    new_meaning += w
-    real_meaning += meaning[i]
+    w = random.choice([s for s in ' '.join(internet_slang.values()).split() if s[0] == slang[i].lower() and s != definition[i] and s not in new_definition])
+    new_definition += w
+    real_definition += definition[i]
     if i < len(slang)-1:
-        length = max(len(w), len(meaning[i])) - min(len(w), len(meaning[i])) + 1
-        if len(w) < len(meaning[i]):
-            new_meaning += ' ' * length
-            real_meaning += ' '
+        num_spaces = max(len(w), len(definition[i])) - min(len(w), len(definition[i])) + 1
+        if len(w) < len(definition[i]):
+            new_definition += ' ' * num_spaces
+            real_definition += ' '
         else:
-            new_meaning += ' '
-            real_meaning += ' ' * length
-
-print(slang + '\n' + real_meaning + '\n' + new_meaning)
+            new_definition += ' '
+            real_definition += ' ' * num_spaces
+if (random.randint(1, 2) == 1):
+    print('1) ' + real_definition)
+    print('2) ' + new_definition)
+else:
+    print('1) ' + new_definition)
+    print('2) ' + real_definition)
